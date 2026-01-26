78107926c348daad5991603f87a6674cda835d92
hyemin <hyemin6647@naver.com>
2026-01-26 23:06:54 +0900

    ♻️ Refactor: 소수 나열하기
    소수 리스트에 있는 소수로 나누었을 때 나누어 떨어지지 않으면 소수이다.

--- dingcorithm/1st_week/01_06_find_prime_list_under_number.js
+++ dingcorithm/1st_week/01_06_find_prime_list_under_number.js
@@ -1,11 +1,13 @@
-// 소수는 자기 자신과 1외에는 아무것도 나눌 수 없다.
 function findPrimeListUnderNumber(number) {
   const primeNumberList = Array();
-  primeNumberList.push(1);
-  //i =2부터 20까지 소수인가? i가 소수라면 primeNumberList에 넣어라.
+  
   for (let i = 2; i < number + 1; i++) {
+    //만약 i=19 라면
     let isPrimeNumber = true;
-    for (let j = 2; j < i; j++) {
+    //현재 j= 2~18 까지 모두 비교를 해주고 있음
+    // 19가 만약에 2와 3으로 나누어떨어지지 않는다면 6으로도 나누어 떨어지지 않을 것임.
+    for (const j of primeNumberList) {
+      // 소수 리스트에 있는 소수로 나누었을 때 나누어 떨어지지 않으면 i도 소수임.
       if (i % j === 0) isPrimeNumber = false;
     }
     if (isPrimeNumber === true) {
