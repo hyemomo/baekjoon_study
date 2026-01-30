5feadb808479ef3afc8c80162902797485a66921
hyemin <hyemin6647@naver.com>
2026-01-30 14:23:42 +0900

    ✨링크드리스트의 합

---
+++ dingcorithm/2nd_week/02_06_get_linked_list_sum.js
@@ -0,0 +1,86 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+class LinkedList {
+  constructor(value) {
+    this.head = new Node(value);
+  }
+  append(value) {
+    let current = this.head;
+    while (current.next !== null) {
+      current = current.next;
+    }
+    current.next = new Node(value);
+  }
+  printAll() {
+    let current = this.head;
+    while (current !== null) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+  getNode(index) {
+    let count = 0;
+    let current = this.head;
+    while (current) {
+      if (index === count) break;
+      current = current.next;
+      count += 1;
+    }
+    return current;
+  }
+
+  addNode(index, value) {
+    let new_node = new Node(value);
+    if (index === 0) {
+      new_node.next = this.head;
+      this.head = new_node;
+      return;
+    }
+    let prev = this.getNode(index - 1);
+    new_node.next = prev.next;
+    prev.next = new_node;
+  }
+  // index번째 노드 삭제하기
+  deleteNode(index) {
+    if (index === 0) {
+      this.head = this.head.next;
+      return;
+    }
+    let current = this.getNode(index);
+    let prev = this.getNode(index - 1);
+    prev.next = current.next;
+  }
+}
+
+const getLinkedListSum = (list1, list2) => {
+  let sum1 = 0;
+  let cur1 = list1.head;
+  while (cur1) {
+    sum1 = sum1 * 10;
+    sum1 += cur1.data;
+    cur1 = cur1.next;
+  }
+
+  let sum2 = 0;
+  let cur2 = list2.head;
+  while (cur2) {
+    sum2 = sum2 * 10;
+    sum2 += cur2.data;
+    cur2 = cur2.next;
+  }
+
+  return sum1 + sum2;
+};
+
+let list1 = new LinkedList(6);
+list1.append(7);
+list1.append(8);
+
+let list2 = new LinkedList(3);
+list2.append(5);
+list2.append(4);
+console.log(getLinkedListSum(list1, list2));
