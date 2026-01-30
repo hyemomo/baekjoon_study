725089afe6b7628db13ea65ced05d92fd623b88b
hyemin <hyemin6647@naver.com>
2026-01-30 16:25:28 +0900

    ➕ 이진 탐색을 사용할 수 없는 경우 - 무작위 정렬

---
+++ dingcorithm/2nd_week/02_09_is_existing_target_number_binary_2.js
@@ -0,0 +1,8 @@
+const finding_target = 2;
+const finding_numbers = [0, 3, 5, 6, 1, 2, 4];
+const is_exist_target_number_binary = (target, array) => {
+// ❌무작위로 정렬되어있는 배열에서는 이진 탐색을 사용할 수 없다!!
+};
+
+const result = is_exist_target_number_binary(finding_target, finding_numbers);
+console.log(result)
