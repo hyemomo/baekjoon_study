5068c5dbefff376fbe606cdaa79951c69e993ae6
hyemin <hyemin6647@naver.com>
2026-02-22 21:36:36 +0900

    ✨딕셔너리 충돌을 해결을 위해 링크드 튜플 클래스 구현

---
+++ dingcorithm/3rd_week/03_10_dict_chaining.js
@@ -0,0 +1,31 @@
+//console.log(my_dicr.get("333")) 이걸 찾으려고 할 때
+// 1. hash(333) % this.items.length -> index+1
+// 2. this.items[1] -> ['333', 7] -> ["77", 6]
+// 3. self.items[1] 에 있는 링크드 리스트에서 333 키에 해당하는 value를 반환하면 된다.
+//=> value 는 7이야
+
+class LinkedTuple {
+  constructor() {
+    this.items = [];
+  }
+  //Python: (key, value) → 튜플
+  //JavaScript: (key, value) → 콤마 연산자 (마지막 값만 반환)
+  //따라서 () 대신 [] 이나 {}를 사용했다.
+  add(key, value) {
+    this.items.push([key, value]);
+  }
+  get(key) {
+    for (let [k, v] of this.items) {
+      if (key === k) {
+        return v;
+      }
+    }return undefined
+  }
+}
+const bucket = new LinkedTuple();
+bucket.add("333", 7);
+bucket.add("77", 6);
+
+console.log(bucket.get("333")); // 7
+console.log(bucket.get("77"));  // 6
+console.log(bucket.get("999")); // undefined
